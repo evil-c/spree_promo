bebf85b7f47c80cd49e1fe7f15f3f591e89edfb7
Clive <cwalmsley@nyc.rr.com>
2011-07-29 16:51:57 -0400

    don't care about redeemers

--- app/models/promotion/rules/sponsor.rb
+++ app/models/promotion/rules/sponsor.rb
@@ -1,6 +1,5 @@
 class Promotion::Rules::Sponsor < PromotionRule
   belongs_to :contact
-  has_and_belongs_to_many :redeemers, :join_table => 'promotion_rules_users', :foreign_key => 'promotion_rule_id'
 
   def eligible?(order)
     true
